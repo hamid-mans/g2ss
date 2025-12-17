cce748d0be51c15eeec31df8901e4571a1f1d4d9
hamid <mansouri.hamid23@gmail.com>
2025-12-17 19:11:33 +0100

    now

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/', 'app.home.')]
 class HomeController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', 'app.home.index')]
     public function index(): Response
     {
         if(!$this->getUser())
