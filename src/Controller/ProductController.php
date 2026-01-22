35194b5b391df2ea8319022a44a284f84a1485b7
hamid <mansouri.hamid23@gmail.com>
2026-01-22 10:40:59 +0100

    ProductUnit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Form\SearchType;
+use App\Repository\DepositRepository;
 use App\Repository\ProductRepository;
 use App\Repository\ProductUnitRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,10 +24,40 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 final class ProductController extends AbstractController
 {
     #[Route('', name: 'index')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(PaginatorInterface $paginator, ProductRepository $productRepository, Request $request): Response
     {
+        $searchForm = $this->createForm(SearchType::class, null, [
+            'method' => 'GET',
+        ]);
+        $searchForm->handleRequest($request);
+
+        $query = $productRepository->createQueryBuilder('p')
+            ->orderBy('p.refInterne', 'ASC');
+
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+            $filters = $searchForm->getData();
+
+            if (!empty($filters['search'])) {
+                $query
+                    ->orWhere('p.refInterne LIKE :refInterne')
+                    ->orWhere('p.refSupplier LIKE :refSupplier')
+                    ->orWhere('p.designation LIKE :designation')
+                    ->setParameter('refInterne', '%' . $filters['search'] . '%')
+                    ->setParameter('refSupplier', '%' . $filters['search'] . '%')
+                    ->setParameter('designation', '%' . $filters['search'] . '%');
+            }
+        }
+
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('dashboard/product/index.html.twig', [
             'products' => $productRepository->findAll(),
+            'searchForm' => $searchForm->createView(),
+            'pagination' => $pagination
         ]);
     }
 
@@ -55,7 +89,7 @@ final class ProductController extends AbstractController
     }
 
     #[Route('{refInterne}', name: 'update')]
-    public function update(#[MapEntity(mapping: ['refInterne' => 'refInterne'])] Product $product, ProductUnitRepository $productUnitRepository, Request $request, EntityManagerInterface $entityManager): Response
+    public function update(#[MapEntity(mapping: ['refInterne' => 'refInterne'])] Product $product, DepositRepository $depositRepository, PaginatorInterface $paginator, ProductUnitRepository $productUnitRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ProductType::class, $product, [
             'submit_label' => '<i class="save icon"></i> Enregistrer',
@@ -83,13 +117,47 @@ final class ProductController extends AbstractController
             $sommeUnit += $unit->getBuyPrice();
         }
 
-        $pmp = $sommeUnit / count($productUnits);
+        $pmp = $sommeUnit / (count($productUnits) > 0 ? count($productUnits) : 1);
+
+
+        // STOCK
+
+        $searchForm = $this->createForm(SearchType::class, null, [
+            'method' => 'GET',
+        ]);
+        $searchForm->handleRequest($request);
+
+        $query = $productUnitRepository->createQueryBuilder('p')
+            ->andWhere('p.product = :product')
+            ->setParameter('product', $product)
+            ->orderBy('p.serialNumber', 'ASC');
+
+        $deposits = $depositRepository->findBy(['company' => $this->getUser()->getCompany()], ['name' => 'ASC']);
+
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+            $filters = $searchForm->getData();
+
+            if (!empty($filters['search'])) {
+                $query
+                    ->andWhere('p.serialNumber LIKE :serialNumber')
+                    ->setParameter('serialNumber', '%' . $filters['search'] . '%');
+            }
+        }
+
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
 
         return $this->render('dashboard/product/update.html.twig', [
             'product' => $product,
             'form' => $form,
             'productUnits' => $productUnits,
             'pmp' => $pmp,
+            'searchForm' => $searchForm->createView(),
+            'pagination' => $pagination,
+            'deposits' => $deposits,
         ]);
     }
 
